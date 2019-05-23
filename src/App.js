7de8f80ace18c0073bf38ab07cd1d5ea3968387d
Robin Wieruch <wrobin@gmx.net>
2019-05-23 10:30:58 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
 const App = () => {
-  const handleClick = () => console.log('Clicked!');
-
   return (
     <div>
-      <button type="button" onClick={handleClick}>
-        Click me!
-      </button>
+      <Button handleClick={() => console.log('Clicked button One!')}>
+        Click Button One!
+      </Button>
+      <Button
+        type="submit"
+        handleClick={() => console.log('Clicked button Two!')}
+      >
+        Click Button Two!
+      </Button>
     </div>
+  );
+};
+
+const Button = ({ type = 'button', handleClick, children }) => {
+  return (
+    <button type={type} onClick={handleClick}>
+      {children}
+    </button>
   );
 };
 
